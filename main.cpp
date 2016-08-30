ff3f3d995e0f7dfe9662e92f9de29da50794f0ca
saabhugin <ola.harkegard@saabgroup.com>
2016-08-30 13:39:59 +0200

    Updated Main.cpp
    Pre merge updates.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
  * 
  * Finally the sensor values and PWM/SBUS readings is sent to main.elf via UDP. 
  */
- 
 
 #include <stdio.h>
 #include <unistd.h>
@@ -18,19 +17,10 @@
 #include "i2cfunc.h"
 #include "bsocket.h"
 #include "rcreader.h"
-#include "VC01.h"
 #include "PCA9685.h"
 #include "GPIO.h"
 
-// to use the timer class, uncomment timer.h and remove <time.h>
-//#include "timer.h"
-#include<time.h>
-
-
 int main(){	
-	// Mode settings
-	bool debug = 1;			// true --> printing signals while running
-	
 	// Create socket and set listen time out
 	bsocket sock(65536);
 	sock.set_listen_timeout(1000);
@@ -71,32 +61,18 @@ int main(){
 	double gyro_d[3];
 	double mag_d[3];
 	
-	// Init VC01
-	VC01 distance_sensor(1);
-	// Declare values to hold VC01 
-	double distance[1]; 
-	
 	// Init LED
 	int light  = HIGH;
 	int led_counter = 0;
 	GPIO led("30");
-	led.init(OUT, light);		// direction = OUT, value = HIGH
+	led.init(OUT, light);		// direction = OUT, value = light
 	
 	// Init PWM 
 	PCA9685 pwm_out(i2c_open(1, 0x40));
 	usleep(50000);
 	pwm_out.init(50);						// frequency 50 Hz 
-	//GPIO OE("49");						// OE is controlled by a button at the moment, this can be useful later 
-	//OE.init(OUT, LOW);
 	double ctrl_signal[4] = {0,1,0,0};	// initial control signal to the servos
 	pwm_out.signal(ctrl_signal);
-	
-	// Init TIMER
-	/*if(debug) {
-	timer Timer;		// declare more objects of class timer if you wish to clock several things at the same time
-	}*/
-	
-	// INITIALIZATION COMPLETED
 	
 	// Flushing UDP buffers
 	for(int i=0; i<100; i++) {
@@ -114,6 +90,7 @@ int main(){
 	printf("Initial Data size: %d \n\n", data_size);
 	usleep(1000000);
 	
+	// INITIALIZATION COMPLETED
 	printf("Hugin program started!\n");
 	
 	while(1){
@@ -124,39 +101,34 @@ int main(){
 			
 			// Pace keeper packet received (i.e. flag)
 			if(socket_vals[0].i_vals[0]){				
-				/*if(debug) {
-					Timer.start();
-				}*/
 				socket_vals[0].i_vals[0] = 0;		// reset the flag, i_vals accesses the ready signal				
 				// Set PWM outputs
-				// socket is sometimes 0.00, we do not want to send that to the servos (why is it 0?)
-				
-				/*if(socket_vals[1].d_vals[0] > 0.01){				//d_vals acceses the motor signals from Simulink
-					pwm_out.signal_s(socket_vals[1].d_vals[0], 0);	//sending socket signal 0 to servo 0 
-					pwm_out.signal_s(sbus_channels_d[0], 1);		//sending sbus signal 0 to servo 1 for time delay comparison (not through simulink)
-				}*/
-				pwm_out.signal(socket_vals[1].d_vals);
-
-				
+				// Test to use throttle as output for every motor
+				for(int i= 0; i<4; i++){
+					ctrl_signal[i]=socket_vals[1].d_vals[2];
+					// socket is sometimes 0.00, we do not want to send that to the servos (why is it 0?)				
+					if (ctrl_signal[i] < 0.01){
+						ctrl_signal[i]=0.01;
+					}
+				}
+				pwm_out.signal(ctrl_signal);
+							
 				// Get IMU readings
 				imu.get_accelerations(acc_d);
 				imu.get_angular_velocities(gyro_d);	
 				mag.get_magnetometer_data(mag_d);
-				
-				// Get RC readings (SBUS)
-				rcr.parse_SBus(channels, sbus_channels_d);
-				
-				// Get distance values
-				distance[0] = distance_sensor.get_distance();
-				
+								
 				// Get RC readings (PWM)
 				rcr.get_readings(channels);
 
 				for(int i = 0; i < 4; i++) {
+					// Scale PWM signal from range SERVOMIN-SERVOMAX to 0-1.
 					pwm_readings_d[i] = ((double)channels[i]-SERVOMIN)/(SERVOMAX-SERVOMIN);	
 				}
 				
-				
+				// Get RC readings (SBUS)
+				rcr.parse_SBus(channels, sbus_channels_d);
+							
 				// printing signals for comparison if debug = true
 				if(debug) {
 					for(int i = 0; i < 2; i++) {		// decide how many signals you want to print by changing upper limit for i (1,2,3,4)
@@ -171,7 +143,6 @@ int main(){
 				sock.send(LOCAL_HOST, 22001, acc_d, 3);
 				sock.send(LOCAL_HOST, 22002, gyro_d, 3);
 				sock.send(LOCAL_HOST, 22003, mag_d, 3);
-				sock.send(LOCAL_HOST, 22006, distance, 1);
 				sock.send(LOCAL_HOST, 22101, sbus_channels_d, num_sbus_channels);
 				sock.send(LOCAL_HOST, 22102, pwm_readings_d, num_pwm_channels);
 								
@@ -182,12 +153,6 @@ int main(){
 					led.write(light);
 					led_counter = 0;
 				}
-				
-				/*if(debug) {
-					// Stop timer and print total elapsed time
-					Timer.stop();
-					printf("Elapsed time (ms): %f \n\n", Timer.millis());
-				} */
 			}
 		}
 	}
