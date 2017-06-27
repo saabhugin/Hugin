8e683287cd7fbcbba7f3b1323229995b6d532f33
saabhugin <ola.harkegard@saabgroup.com>
2017-06-27 09:05:55 +0200

    Added Octave-script for plotting Simulink output.

---
+++ octave/plotAccelGyroEuler.m
@@ -0,0 +1,49 @@
+clear; clc; close all;
+addpath ../logfiles
+load main.mat
+
+%% Extract the data from yout_rt
+time = yout_rt.time;
+acc= squeeze(yout_rt.signals(1).values);
+gyr = squeeze(yout_rt.signals(2).values);
+euler = squeeze(yout_rt.signals(3).values);
+
+%% Accelerometer plot
+figure('Name','Accelerometer')
+ax1=subplot(3,1,1);
+stairs(time, acc(1,:))
+legend('x')
+ax2=subplot(3,1,2);
+stairs(time, acc(2,:))
+legend('y')
+ax3=subplot(3,1,3);
+stairs(time, acc(3,:))
+legend('z')
+linkaxes([ax1 ax2 ax3],'xy')
+axis([min(time) max(time) 1.1*min(min(acc(:,:))) 1.1*max(max(acc(:,:)))])
+
+%% Gyro plot
+figure('Name','Gyro')
+ax1=subplot(3,1,1);
+stairs(time, 180/pi()*gyr(1,:))
+legend('p')
+ax2=subplot(3,1,2);
+stairs(time, 180/pi()*gyr(2,:))
+legend('q')
+ax3=subplot(3,1,3);
+stairs(time, 180/pi()*gyr(3,:))
+legend('r')
+linkaxes([ax1 ax2 ax3],'xy')
+axis([min(time) max(time) 1.1*180/pi()*min(min(gyr(:,:))) 1.1*180/pi()*max(max(gyr(:,:)))])
+
+%% Euler angles plot
+figure('Name','Euler angles')
+ax1=subplot(3,1,1);
+stairs(time, 180/pi()*euler(1,:))
+legend('yaw')
+ax2=subplot(3,1,2);
+stairs(time, 180/pi()*euler(2,:))
+legend('pitch')
+ax3=subplot(3,1,3);
+stairs(time, 180/pi()*euler(3,:))
+legend('roll')
