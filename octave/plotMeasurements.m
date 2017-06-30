59603170fe787600ecd51c5740e6ac41d54127ea
saabhugin <ola.harkegard@saabgroup.com>
2017-06-30 16:12:03 +0200

    Updated plotfkn

--- octave/plotMeasurements.m
+++ octave/plotMeasurements.m
@@ -23,7 +23,37 @@ legend('z')
 linkaxes([ax1 ax2 ax3],'xy')
 axis([min(time) max(time) 1.1*min(min(acc(:,:))) 1.1*max(max(acc(:,:)))])
 
-%% Gyro plot
+
+if (version == "org")
+%% Gyro plot original hugin
+figure('Name','Gyro')
+ax1=subplot(3,1,1);
+stairs(time, gyr(1,:))
+legend('p')
+ax2=subplot(3,1,2);
+stairs(time, gyr(2,:))
+legend('q')
+ax3=subplot(3,1,3);
+stairs(time, gyr(3,:))
+legend('r')
+linkaxes([ax1 ax2 ax3],'xy')
+%axis([min(time) max(time) 1.1*180/pi()*min(min(gyr(:,:))) 1.1*180/pi()*max(max(gyr(:,:)))])
+
+%% Magnotometer plot
+figure('Name','Magnetometer')
+ax1=subplot(3,1,1);
+stairs(time, euler(1,:))
+legend('x')
+ax2=subplot(3,1,2);
+stairs(time, euler(2,:))
+legend('y')
+ax3=subplot(3,1,3);
+stairs(time, euler(3,:))
+legend('z')
+end
+
+if (version == "new")
+%% Gyro plot new hugin
 figure('Name','Gyro')
 ax1=subplot(3,1,1);
 stairs(time, 180/pi()*gyr(1,:))
@@ -48,6 +78,8 @@ legend('pitch')
 ax3=subplot(3,1,3);
 stairs(time, 180/pi()*euler(3,:))
 legend('roll')
+
+end
 
 %% Extract the data from yout_rt
 rc= squeeze(yout_rt.signals(7).values);
